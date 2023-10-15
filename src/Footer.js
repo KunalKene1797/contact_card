5555337679169869c4d19b45dc5675fdd79768d2
kunalkene1797 <kunalkene1797@gmail.com>
2023-10-14 23:56:15 -0500

    Updates

--- src/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return <div className="footer-section">
-    <div className="footer-text">
-    <p className="footer-madewith">Made with</p> <p className="footer-heart">❤</p><p className="footer-using">using React</p>
-    </div>
-    
-  </div>;
-}
--- src/components/Lens.js
+++
@@ -1,3 +0,0 @@
-export default function Lens(){
-    return <h1>Lens</h1>
-}
